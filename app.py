849d3d91bc4e9ed15d00e23facb3da4a7199da5d
Plorthos <tlkb@protonmail.com>
2023-08-10 20:31:37 -0400

    changed app to work with test data

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
+from datetime import datetime
 from flask import Flask, request, render_template
 import csv
-from io import TextIOWrapper
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -8,14 +8,19 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password@localhost:3306/mydb'
 db = SQLAlchemy(app)
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+class Shipment(db.Model):
+    sku = db.Column(db.Integer, primary_key=True)
+    quantity = db.Column(db.Integer)
+    shippingtime = db.Column(db.Integer)
+    carrier = db.Column(db.String(45))
+    shippingcost = db.Column(db.Float)
+    location = db.Column(db.String(255))
+    inspection = db.Column(db.String(255))
+    transportmode = db.Column(db.String(255))
+    route = db.Column(db.String(255))
 
     def __repr__(self):
         return f'<User {self.username}>'
-
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_csv():
@@ -32,12 +37,43 @@ def upload_csv():
     file.save("temp.csv")
     with open("temp.csv") as csvfile:
         csv_reader = csv.reader(csvfile)
-        # remove header
-        next(csv_reader)
+        # map header to row
+        sku = quantity = shipping_time = carrier = \
+            shipping_cost = location = inspection = transport_mode = route = -1
+        headers = next(csv_reader)
+        for header, i in zip(headers, range(len(headers))):
+            if header.lower() == "sku":
+                sku = i
+            elif header.lower() == "number of products sold":
+                quantity = i
+            elif header.lower() == "shipping times":
+                shipping_time = i
+            elif header.lower() == "shipping carriers":
+                carrier = i
+            elif header.lower() == "shipping costs":
+                shipping_cost = i
+            elif header.lower() == "location":
+                location = i
+            elif header.lower() == "inspection results":
+                inspection = i
+            elif header.lower() == "transportation modes":
+                transport_mode = i
+            elif header.lower() == "routes":
+                route = i
 
         for row in csv_reader:
-            new_user = User(username=row[0], email=row[1])
-            db.session.add(new_user)
+            new_record = Shipment(
+                sku = row[sku],
+                quantity = row[quantity],
+                shippingtime = row[shipping_time],
+                carrier = row[carrier],
+                shippingcost = row[shipping_cost],
+                location = row[location],
+                inspection = row[inspection],
+                transportmode = row[transport_mode],
+                route = row[route]
+            )
+            db.session.add(new_record)
         db.session.commit()
     return 'CSV data inserted into database'
 
