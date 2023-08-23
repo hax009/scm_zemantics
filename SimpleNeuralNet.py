abd3e574e68ed13815500158e81fa3883c378719
Plorthos <tlkb@protonmail.com>
2023-08-22 23:21:10 -0400

    update date prediction model with distance

--- SimpleNeuralNet.py
+++ SimpleNeuralNet.py
@@ -5,10 +5,17 @@ class NeuralNetwork(nn.Module):
     def __init__(self, input_dim, hidden_dim):
         super(NeuralNetwork, self).__init__()
         self.fc1 = nn.Linear(input_dim, hidden_dim)
-        self.fc2 = nn.Linear(hidden_dim, 1)
+        self.dropout1 = nn.Dropout(0.15)
+        self.fc2 = nn.Linear(hidden_dim, 16)
+        self.dropout2 = nn.Dropout(0.1)
+        self.fc3 = nn.Linear(16, 1)
     
     def forward(self, x):
         x = self.fc1(x)
         x = nn.ReLU()(x)
+        x = self.dropout1(x)
         x = self.fc2(x)
+        x = nn.ReLU()(x)
+        x = self.dropout2(x)
+        x = self.fc3(x)
         return x
